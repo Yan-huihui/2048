a5de96d033419028b1f978480d6823aa33ccec42
Yan-huihui <100505176+Yan-huihui@users.noreply.github.com>
2022-02-28 20:50:26 +0800

    添加注释

--- 2048.c
+++ 2048.c
@@ -9,7 +9,7 @@
 int counter;
 int score = 0;
 
-int getch(void)
+int getch(void)									//输入一个字符刷新缓冲区
 {
     struct termios oldattr, newattr;
     int ch;
@@ -22,7 +22,7 @@ int getch(void)
     return ch;
 }
 
-void print2048(int (*arry)[4], int rows)
+void print2048(int (*arry)[4], int rows)		//按照格式打印二维数组
 {
 	printf("---------------------\n");
 	int i, j, clos = sizeof(arry[0]) / sizeof(arry[0][0]);
@@ -44,7 +44,7 @@ void print2048(int (*arry)[4], int rows)
 	}
 }
 
-void remove_right(int (*arry)[4],int rows)
+void remove_right(int (*arry)[4],int rows)		//向右移动，数组内数字对应改变
 {
 	int i, j, clos = sizeof(arry[0]) / sizeof(arry[0][0]);
 	for(i = 0; i < rows ; ++i)//向右移动
@@ -62,7 +62,7 @@ void remove_right(int (*arry)[4],int rows)
 			}
 		}
 
-		for(j = 3; j > 0; --j)
+		for(j = 3; j > 0; --j)					//移动后，前后左右两个有相同的前数乘二，后数归零
 		{
 			if(arry[i][j] == arry[i][j - 1])
 			{
@@ -70,12 +70,12 @@ void remove_right(int (*arry)[4],int rows)
 				arry[i][j - 1] = 0;
 				if(arry[i][j] != 0)
 				{
-					++score;
+					++score;					//消除数字时分数加1
 				}
 			}
 		}
 
-		for(t = 0;t < 3; ++t)
+		for(t = 0;t < 3; ++t)					//防止消除相同数字后中间有空位，再次向右移动
 		{
 			for(j = 3; j > 0; --j)
 			{
@@ -89,7 +89,7 @@ void remove_right(int (*arry)[4],int rows)
 	}
 }
 
-void remove_left(int (*arry)[4],int rows)
+void remove_left(int (*arry)[4],int rows)		//向左对应操作
 {
 	int i, j, clos = sizeof(arry[0]) / sizeof(arry[0][0]);
 	for(i = 0; i < rows; ++i)//向左移动
@@ -134,7 +134,7 @@ void remove_left(int (*arry)[4],int rows)
 	}
 }
 
-void remove_up(int (*arry)[4],int rows)
+void remove_up(int (*arry)[4],int rows)					//向上对应操作
 {
 	int i, j, clos = sizeof(arry[0]) / sizeof(arry[0][0]);
 	for(j = 0; j < clos; ++j)//向上移动
@@ -179,7 +179,7 @@ void remove_up(int (*arry)[4],int rows)
 	}
 }
 
-void remove_down(int (*arry)[4],int rows)
+void remove_down(int (*arry)[4],int rows)					//向下对应操作
 {
 	int i, j, clos = sizeof(arry[0]) / sizeof(arry[0][0]);
 	for(j = 0; j < clos; ++j)//向下移动
@@ -223,7 +223,7 @@ void remove_down(int (*arry)[4],int rows)
 		}
 	}
 }
-int random2or4(void)
+int random2or4(void)					//返回一个2或者4
 {
 	if(random() % 2 == 0)
 	{
@@ -235,12 +235,12 @@ int random2or4(void)
 	}
 }
 
-int randomLess4(void)
+int randomLess4(void)					//返回一个小于4的值
 {
 	return (random() % 4);
 }
 
-int isSame(int (*arry)[4])
+int isSame(int (*arry)[4])				//判断每个位置相邻的数是否有相同的，有相同的返回1，没有相同返回0
 {
 	int i, j;
 	for(i = 0; i < 4; ++i)
@@ -265,7 +265,7 @@ int isSame(int (*arry)[4])
 	}
 	return 0;
 }
-int  winOrLoseStop(int (*arry)[4])
+int  winOrLoseStop(int (*arry)[4])				//判断是否输或者赢，赢返回1，输返回-1，继续游戏返回0
 {
 	int i, j;
 	counter = 0;
@@ -290,7 +290,7 @@ int  winOrLoseStop(int (*arry)[4])
 	return 0;
 }
 
-int copyarry(int (*des)[4], int (*src)[4])
+int copyarry(int (*des)[4], int (*src)[4])						//用于拷贝数组
 {
 	int i,j;
 	for(i = 0; i < 4; ++i)
@@ -303,7 +303,7 @@ int copyarry(int (*des)[4], int (*src)[4])
 	return 0;
 }
 
-int isSame_OldAndNewArry(int (*old)[4], int (*new)[4])
+int isSame_OldAndNewArry(int (*old)[4], int (*new)[4])			//用于判断两个二维数组是否相同
 {
 	int i,j;
 	for(i = 0; i < 4; ++i)
@@ -324,7 +324,7 @@ int  start(void)
 	int arry[4][4] = {0};
 	int i = randomLess4();
 	int j = randomLess4();
-	arry[i][j] = random2or4();
+	arry[i][j] = random2or4();							//游戏开始在随机位置写入一个2或者4
 	int rows = sizeof(arry) / sizeof(arry[0]);
 
 	print2048(arry, rows);
@@ -334,33 +334,33 @@ int  start(void)
 		int buff[4][4] = {0};
 		copyarry(buff, arry);
 
-		char ch = getch();
-		if(ch == 'd')
+		char ch = getch();								//获取从键盘输入字符
+		if(ch == 'd')									//若输入d执行向右相关操作
 		{
 			remove_right(arry, rows);
 		}
-		else if(ch == 'a')
+		else if(ch == 'a')								//若输入a执行向左相关操作
 		{
 			remove_left(arry, rows);
 		}
 		else if(ch == 'w')
 		{
-			remove_up(arry, rows);
+			remove_up(arry, rows);						//若输入w执行向上相关操作
 		}
 		else if(ch == 's')
 		{
-			remove_down(arry, rows);
+			remove_down(arry, rows);					//若输入s执行向下相关操作
 		}
-		else if(ch == 'q')
+		else if(ch == 'q')								//若输入q退出游戏
 		{
 			return 0;
 		}
-		else
+		else											//输入其他字符进入下一个循环
 		{
 			continue;
 		}
 
-		system("clear");
+		system("clear");								//清屏
 		int sameOldAndNew = isSame_OldAndNewArry(buff, arry);
 		do
 		{
@@ -368,25 +368,22 @@ int  start(void)
 			j = randomLess4();
 			winOrLoseStop(arry);
 		}
-		while(arry[i][j] != 0 && counter < 16);
+		while(arry[i][j] != 0 && counter < 16);			
 
-		if(counter < 16 && sameOldAndNew == 1)
+		if(counter < 16 && sameOldAndNew == 1)			//在数组未满以及移动前后数组不相同情况下找不为零位置写入2或者4
 		{
 			arry[i][j] = random2or4();
 		}
 		
-		printf("score   %d\n",score);
-		printf("counter %d\n",counter);//
-		printf("same    %d\n",isSame(arry));//
-		printf("winOrLoseStop %d\n",winOrLoseStop(arry) );//
-		print2048(arry, rows);
+		printf("score   %d\n",score);					//打印分数
+		print2048(arry, rows);							//打印移动后二维数组
 
-		if(winOrLoseStop(arry) == 1)
+		if(winOrLoseStop(arry) == 1)					//判断是否赢
 		{
 			printf("win\n");
 			break;
 		}
-		if(winOrLoseStop(arry) == -1)
+		if(winOrLoseStop(arry) == -1)					//判断是否输
 		{
 			printf("lose\n");
 			break;
